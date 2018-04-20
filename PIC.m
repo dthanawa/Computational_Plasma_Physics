a25587bfe715d798c8c702dc702f693531c72116
Dhruv Thanawala <dthanawa@mtu.edu>
2018-04-20 14:43:56 -0400

    PIC 1D weight fraction and hx works with random

--- PIC.m
+++ PIC.m
@@ -5,61 +5,36 @@ Ni = 8;
 N = Ne + Ni;
 dxe = 2/Ne;
 dxi = 1/Ni;
+
 for i=1:Ni
     xi(i) = (i-0.5)*dxi;
     vi(i) = 0;
 end
+
 for i=1:Ne/2
     xe(i) = (i-0.5)*dxe;
     ve(i) = 0.5 + 0.1*sin(2*pi*xe(i));
 end
+
 for i=(Ne/2)+1 : Ne
     xe(i) = xe(i-Ne/2);
     ve(i) = -0.5 - 0.1*sin(2*pi*xe(i));
 end
+
 V = [vi';ve'];
-X = [xi';xe']
-xe = sort(xe');
+X = [xi';xe'];  
 node = linspace(0,1,5);
-i=0.0;
-t = 1;
-j=1;
-    while( j <= Ne )
-        
-       if(xe(j)<i)
-            c=0;
-            we(j,1) = abs(xe(j)-node(t))
-            we(j,2) = (1/(length(node)-1)) - we(j,1)
-            
-       else
-            c=1;
-            j = j-1
-            i=i+(1/(length(node)-1))
-            t=t+c;
-            if(i>1)
-                break;
-            end
-       end
-        j=j+1;
-    end
-i=0.0;
-t = 1;
-j=1;
-    while( j <= Ne )
-        
-       if(xi(j)<i)
-            c=0;
-            wi(j,1) = abs(xi(j)-node(t))
-            wi(j,2) = (1/(length(node)-1)) - wi(j,1)
-            
-       else
-            c=1;
-            j = j-1
-            i=i+(1/(length(node)-1))
-            t=t+c;
-            if(i>1)
-                break;
-            end
-       end
-        j=j+1;
-    end
+dn = node(2)-node(1);
+
+for i = 1:N
+    n(i) = ceil(X(i)/dn);
+    hx(i,1) =  X(i) - (n(i)-1)*dn;
+    hx(i,2) = n(i)*dn - X(i);
+end
+
+for i = 1:N
+    w(i,1) = hx(i,1)/(hx(i,1)+hx(i,2));
+    w(i,2) = 1-w(i,1);
+end
+
+
