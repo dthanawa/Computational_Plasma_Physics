53c4ef1a9f300a9cd13b8b3d068eab4b67f21145
Dhruv Thanawala <dthanawa@mtu.edu>
2018-05-30 01:52:25 -0400

    malloc

--- PIC.c
+++ PIC.c
@@ -3,21 +3,28 @@
 #include<math.h>
 #include <time.h>
 #define PI (3.141592653589793)
-#define N (100)
-#define num_node (5)
+//int N = 100;
+//int num_node = 5;
 
 // define the user-defined functions
 // Electric Potential
-float get_phi(float qn[]);
+double get_phi(double qn[]);
 
 // Electric Field
-float get_field_pic(float phi[],float dn);
+double get_field_pic(double phi[],double dn);
 
 // Linear Interpolation 
-float interp_linear( float X[], float E[], float dn,float n[] );
+double interp_linear( double X[], double E[], double dn,double n[] );
 
 // Global variables
-float x[num_node-2]  = {{ 0.0} }, E[num_node]  = { {0.0} }, Ex[N] = { {0.0} };
+double x*;
+double E*;
+double Ex*;
+int N = 100,num_node = 5;
+x = (double *)malloc((num_node-2) * sizeof(double));
+E = (double *)malloc(num_node * sizeof(double));
+Ex = (double *)malloc(N * sizeof(double));
+x[num_node-2]  = 0.0; E[num_node]  = 0.0; Ex[N] = 0.0;
 
 // Main Function
 int main()
@@ -27,19 +34,36 @@ int main()
 
     int Ni = 50, Ne = 50;// N;
     int i, j;    
-    float t, a = 0.0, b = 1.0;
-    float dxe, dxi, m = 1.0, node['p'] = { 0.0 }, k = 0.0, mi = 1000.0, me = 1.0;
+    double t, a = 0.0, b = 1.0;
+    //double node*,xi*,xe*vi*,ve*,X*,V*,Vnew*,Xnew*,hx*,n*,w*,q*,qn*,phi*;
+   
+    node = (double *)malloc(num_node * sizeof(double));
+    xi = (double *)malloc(N * sizeof(double));
+    xe = (double *)malloc(N * sizeof(double));
+    vi = (double *)malloc(N * sizeof(double));
+    ve = (double *)malloc(N * sizeof(double));
+    X = (double *)malloc(N * sizeof(double));
+    V = (double *)malloc(N * sizeof(double));
+    Vnew = (double *)malloc(N * sizeof(double));
+    Xnew = (double *)malloc(N * sizeof(double));
+    hx = (double *)malloc(N *2* sizeof(double));
+    n = (double *)malloc(N * sizeof(double));
+    w = (double *)malloc(N *2* sizeof(double));
+    q = (double *)malloc(N * sizeof(double));
+    qn = (double *)malloc(num_node * sizeof(double));
+    phi = (double *)malloc(num_node * sizeof(double));
+    double dxe, dxi, m = 1.0, node[num_node] = 0.0, k = 0.0, mi = 1000.0, me = 1.0;
     
     // Memory Preallocation for Velocity, postion, weight fraction, Charge density and Electric Potential
-    float xi[N] = { 0.0 }, xe[N] = { 0.0 }, vi[N] = { 0.0 }, ve[N] = { 0.0 };
-    float X[N] = { 0.0 }, V[N] = { 0.0 }, Vnew[N] = { 0.0 }, Xnew[N] = { 0.0 };
-    float hx[N][2] = { 0.0 }, n[N] = { 0.0 }, w[N][2] = { 0.0 };
-    float q[N] = { 0.0 },qn[num_node] = { 0.0 }, phi[num_node] = { 0.0 };
+    xi[N] = 0.0; xe[N] = 0.0; vi[N] = 0.0; ve[N] = 0.0;
+    X[N] = 0.0; V[N] = 0.0; Vnew[N] = 0.0; Xnew[N] = 0.0;
+    hx[N][2] = 0.0; n[N] = 0.0; w[N][2] = 0.0;
+    q[N] = 0.0; qn[num_node] = 0.0; phi[num_node] = 0.0;
     
     // Time step
-    float dt = 0.0005;
+    double dt = 0.0005;
     
-    float r = 2;
+    double r = 2;
     dxe = 2.0/Ne;
     dxi = 1.0/Ni;
     
@@ -72,18 +96,18 @@ int main()
         {
             X[i] = xi[i] + xe[i];
             V[i] = vi[i] + ve[i];
-            printf("X = %.5f\t\t V = %.5f\n",X[i],V[i]);
+            printf("X = %.15lf\t\t V = %.15lf\n",X[i],V[i]);
         }
     
     // Creating 1D Mesh   
     for(i = 0; i < num_node; i++)
         {
             node[i] = k;
-            printf("node(%d) = %.5f\n",i,node[i]);
+            printf("node(%d) = %.15lf\n",i,node[i]);
             k = k + 1.0/(num_node-1.0);
         }
         
-    float dn = 1.0/(num_node-1.0);
+    double dn = 1.0/(num_node-1.0);
     
     // Time Loop BEGINS
     for(t = 0; t <= 1.5; t+=dt)
@@ -95,27 +119,27 @@ int main()
                     n[i] = floor((X[i] - a)/(b-a)*(num_node - 1)+1);
                     hx[i][1] =  X[i] - (n[i]-1.0)*dn;
                     hx[i][2] = n[i]*dn - X[i];
-                    printf("n = %.5f hx = %.5f  %.5f\n",n[i],hx[i][1],hx[i][2]);
+                    printf("n = %.15lf hx = %.15lf  %.15lf\n",n[i],hx[i][1],hx[i][2]);
                 }
         
             for(i = 1;i<N;i++)
                 {
                     w[i][1] = hx[i][1]/(hx[i][1]+hx[i][2]);
                     w[i][2] = 1.0-w[i][1];
-                    printf("w1 = %.5f  w2 = %.5f\n",w[i][1],w[i][2]);
+                    printf("w1 = %.15lf  w2 = %.15lf\n",w[i][1],w[i][2]);
                 }
             
             // Random Charge generator
             srand((unsigned int)time(NULL));
             
             for(i=0;i<N/2;i++)
-                q[i] = ((float)rand()/(float)(RAND_MAX)) * r;
+                q[i] = ((double)rand()/(double)(RAND_MAX)) * r;
         
             for(i=N/2;i<N;i++)
-                q[i] = ((float)rand()/(float)(RAND_MAX)) * (-r);
+                q[i] = ((double)rand()/(double)(RAND_MAX)) * (-r);
     
             for(i = 1;i<N;i++)
-                printf("q = %.5f \n",q[i]);
+                printf("q = %.15lf \n",q[i]);
             
             // Calculating Charge density
             for(j = 1;j<N;j++)
@@ -125,7 +149,7 @@ int main()
                     qn[(int)n[j]+1] = qn[(int)n[j]+1] +  w[j][1]*q[j];
                 }
             for(i=0;i<num_node;i++)
-                printf("qn = %.5f \n",qn[i]);
+                printf("qn = %.15lf \n",qn[i]);
             
             // Calling Electric Potential Function
             get_phi(qn);
@@ -137,13 +161,13 @@ int main()
             get_field_pic(phi,dn);
         
             for(i = 0;i < num_node; i++)
-                printf("phi = %.5f\t E = %.5f \n",phi[i],E[i]);
+                printf("phi = %.15lf\t E = %.15lf \n",phi[i],E[i]);
             
             // Calling Linear Interpolation Function
             interp_linear( X, E, dn, n );
         
             for(i = 0; i < N; i++)
-                printf("Ex = %.5f \n",Ex[i]);
+                printf("Ex = %.15lf \n",Ex[i]);
             
             // Updating Velocity
             for(i = 0;i < N; i++)
@@ -155,14 +179,14 @@ int main()
                 }
             
             for(i = 0; i < N; i++)
-                printf("Vnew = %.5f \n",Vnew[i]);
+                printf("Vnew = %.15lf \n",Vnew[i]);
             
             // Updating Postion
             for(i = 0; i < N; i++)
                 Xnew[i] = X[i] + dt * Vnew[i]; 
     
             for(i = 0; i < N; i++)
-                printf("Xnew = %.5f \n",Xnew[i]);
+                printf("Xnew = %.15lf \n",Xnew[i]);
     
             for(i = 0; i < N; i++)
                 {
@@ -170,7 +194,7 @@ int main()
                     V[i] = Vnew[i];
                 }
             
-            printf("t = %f\n",t);
+            printf("t = %.lf\n",t);
             
         }    
     
@@ -178,29 +202,35 @@ int main()
 }
 
 // Electric Potential Function
-float get_phi(float qn[])
+double get_phi(double qn[])
 {
-    int n, i, j;
-    float e0 = 8.85;
-    n = num_node-2;
-    float a[num_node-2][num_node-2] = { 0.0 }, f[num_node-2] = { 0.0 }, alfa[num_node-2] = { 0.0 }, bet[num_node-2] = { 0.0 };
+    int tn, i, j;
+    double e0 = 8.85;
+    tn = num_node-2;
+    double a*,f*,alfa*,bet*;
+    a = (double *)malloc((num_node-2) * sizeof(double));
+    f = (double *)malloc((num_node-2) * sizeof(double));
+    alfa = (double *)malloc((num_node-2) * sizeof(double));
+    bet = (double *)malloc((num_node-2) * sizeof(double));
+    a[num_node-2][num_node-2] = 0.0; f[num_node-2] = 0.0; alfa[num_node-2] = 0.0; bet[num_node-2] = 0.0;
+    
     a[0][0] = -2;
     a[0][1] = 1;
     f[0] = qn[1]/e0;
     
-    for(i=1;i<n;i++)
+    for(i=1;i<tn;i++)
         {
             a[i][i-1] = 1;
             a[i][i]   = -2;
-            if(i!=n-1)
+            if(i!=tn-1)
             a[i][i+1] = 1;
             f[i] = qn[i+1]/e0; 
         }
         
-    for(i=0;i<n;i++)
+    for(i=0;i<tn;i++)
         {
-            for(j=0;j<n;j++)
-                printf("%.5f ",a[i][j]);
+           for(j=0;j<tn;j++)
+                printf("%.15lf ",a[i][j]);
         
             printf("\n");
         }
@@ -208,15 +238,15 @@ float get_phi(float qn[])
     alfa[0] = a[0][0];
     bet[0]  = f[0]/a[0][0];
     
-    for(i=1;i<n;i++)
+    for(i=1;i<tn;i++)
     {
         alfa[i] = a[i][i] - (a[i][i-1])*a[i-1][i]/alfa[i-1];
         bet[i] = (f[i] - (a[i][i-1])*bet[i-1])/alfa[i];
     }
     
-    x[n-1] = bet[n-1];
+    x[tn-1] = bet[tn-1];
     
-    for(i = n-2; i >= 0; i--)
+    for(i = tn-2; i >= 0; i--)
         x[i] = bet[i]-(a[i][i+1])*x[i+1]/alfa[i];
     
     for(i = 0;i<3;i++)
@@ -224,7 +254,7 @@ float get_phi(float qn[])
     
     return 0;
 }
-float get_field_pic(float phi[],float dn)
+double get_field_pic(double phi[],double dn)
 {
     int i;
 
@@ -237,10 +267,12 @@ float get_field_pic(float phi[],float dn)
     return 0;
 }
 
-float interp_linear( float X[], float E[], float dn,float n[] )
+double interp_linear( double X[], double E[], double dn,double n[] )
 {
     int i;
-    float slope[num_node-1];
+    double slope*;
+    slope = (double *)malloc((num_node-1) * sizeof(double));
+    double slope[num_node-1] = 0.0;
 
     for(i = 0; i < num_node-1; i++)
         slope[i] = (E[i+1]-E[i])/dn;
